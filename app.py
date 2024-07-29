490364235ec5ae8b1bdd8aade8b43e77e51c3fc1
ilyeso <ilyes.omri@esprit.tn>
2024-07-29 18:03:14 +0100

    Adding graphs to home page

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ import requests
 from joblib import load
 import pandas as pd
 import os
-
+import plotly.graph_objs as go
+import plotly.offline as pyo
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 api_key = os.getenv('ALPHA_API_KEY')
@@ -12,11 +14,13 @@ model = load('model.joblib')
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    data = get_stock_data('NVDA')
+    graphs = create_graphs(data)
+    return render_template('index.html', graphs=graphs)
 
 
 def get_stock_data(symbol):
-    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=NVDA&outputsize=full&apikey={api_key}'
+    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&outputsize=full&apikey={api_key}'
     response = requests.get(url)
 
     if response.status_code == 200:
@@ -25,39 +29,53 @@ def get_stock_data(symbol):
             time_series_data = data['Time Series (Daily)']
         except KeyError:
             try:
-                
                 if 'Error' in data:
-                    print(
-                        f"Error message from Alpha Vantage API: {data['Information']}")
-                    exit(1)  
+                    print(f"Error message from Alpha Vantage API: {data['Information']}")
+                    exit(1)
 
-                
                 time_series_data = data['Default Response']
             except KeyError:
-                print(
-                    "Unexpected response format from Alpha Vantage API. Please update the code to handle the new format.")
-                exit(1)  
+                print("Unexpected response format from Alpha Vantage API. Please update the code to handle the new format.")
+                exit(1)
 
         df = pd.DataFrame.from_dict(time_series_data, orient='index')
-
-        
         df = df[['1. open', '2. high', '3. low', '4. close', '5. volume']]
-
-        
         df = df.reset_index(drop=False).rename(columns={
-            'index': 'date', '1. open': 'open', '2. high': 'high', '3. low': 'low', '4. close': 'close', '5. volume': 'volume'})
-
-       
+            'index': 'date', '1. open': 'open', '2. high': 'high', '3. low': 'low', '4. close': 'close', '5. volume': 'volume'
+        })
         df['date'] = pd.to_datetime(df['date'])
+        df = df.sort_values('date')
 
-        
+        # Filter data for the last 60 days
+        last_60_days = datetime.now() - timedelta(days=60)
+        df = df[df['date'] >= last_60_days]
+
         filename = 'NVDA_stock_data.xlsx'
-        
         df.to_excel(filename, index=False)
         print(f"Data successfully saved to {filename}")
-        return data
+        return df
     else:
         print(f"Error fetching data: {response.status_code}")
+        return pd.DataFrame()
+
+
+def create_graphs(df):
+    graphs = []
+    traces = {
+        'open': 'Open Price',
+        'high': 'High Price',
+        'low': 'Low Price',
+        'close': 'Close Price'
+    }
+    for column, title in traces.items():
+        trace = go.Scatter(x=df['date'], y=df[column], mode='lines', name=title)
+        data = [trace]
+        layout = go.Layout(title=title, xaxis=dict(title='Date'), yaxis=dict(title=title))
+        fig = go.Figure(data=data, layout=layout)
+        graph = pyo.plot(fig, include_plotlyjs=False, output_type='div')
+        graphs.append(graph)
+
+    return graphs
 
 
 @app.route('/predict', methods=['POST'])
@@ -66,9 +84,10 @@ def predict():
     final_features = [pd.Series(features)]
     prediction = model.predict(final_features)
     output = round(prediction[0], 2)
-    return render_template('index.html', prediction_text=f'Predicted Stock Price: ${output}')
-
+    data = get_stock_data('NVDA')
+    graphs = create_graphs(data)
+    return render_template('index.html', prediction_text=f'Predicted Stock Close Price: ${output}', graphs=graphs)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
